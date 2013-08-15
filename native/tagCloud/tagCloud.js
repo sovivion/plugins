309548401a90b924557cd2e9c26f42e60f439272
hujinglin <hujinglin@yahoo.com>
2013-08-15 15:59:41 +0800

    bug

--- native/tagCloud/tagCloud.js
+++ native/tagCloud/tagCloud.js
@@ -33,25 +33,24 @@
         })();
         //类型检测方法
         var isType = function(obj, type) {
-			var toString = Object.prototype.toString,
-				undefined;
-			return(type === "Null" && obj === null) || (type === "Undefined" && obj === undefined) || toString.call(obj).slice(8, -1) === type;
-		};
+            var toString = Object.prototype.toString;
+            return(type === "Null" && obj === null) || (type === "Undefined" && obj === undefined) || toString.call(obj).slice(8, -1) === type;
+        };
         //深度拷贝方法
         var deepCopy = function(result, source) {
-			for(var key in source) {
-				var copy = source[key];
-				if(result === copy) continue;
-				if(isType(copy, "Array")) {
-					result[key] = arguments.callee(result[key] || [], copy);
-				} else if (isType(copy, "Object")) {
-					result[key] = arguments.callee(result[key] || {}, copy);
-				} else {
-					result[key] = copy;
-				}
-			}
-			return result;
-		};
+            for(var key in source) {
+                var copy = source[key];
+                if(result === copy) continue;
+                if(isType(copy, "Array")) {
+                    result[key] = arguments.callee(result[key] || [], copy);
+                } else if (isType(copy, "Object")) {
+                    result[key] = arguments.callee(result[key] || {}, copy);
+                } else {
+                    result[key] = copy;
+                }
+            }
+            return result;
+        };
         //全局变量
         gvar.option = deepCopy(defaultOption, option);
         gvar.sphereOver = false;
